a8c54cc05c9aced15a879da85554c46166d024bd
Eimantas <eim.kasp@gmail.com>
2021-06-18 19:48:46 +0300

    Slick slider customization options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
     // Priskiriame HTML elementui, slick functionaluma
-    $(".bit-slider").slick();
+    $(".bit-slider").slick({
+        slidesToShow: 3,
+        dots: true,
+        centerMode: true,
+        centerPadding: '60px',
+      });
 });
